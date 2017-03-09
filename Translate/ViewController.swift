9002e467d97d6651b215658ac48c70484c7cc063
John Halley <johnhalley1994@gmail.com>
2017-03-09 15:41:02 +0000

    Test 2

--- Translate/ViewController.swift
+++ Translate/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     //var data = NSMutableData()
     
+    //Hello John
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
